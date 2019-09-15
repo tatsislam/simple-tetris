c369f8601fee3006081cdd508eb4b9a479dfdb95
Tanvee <tatsislam@gmail.com>
2019-09-16 00:02:50 +1000

    created a basic front end

---
+++ piece.js
@@ -0,0 +1,78 @@
+class Piece {
+    constructor() {
+        this.blocks_pos;
+        this.rotation = 0;
+    }
+
+    // Draws the piece on the board
+    draw() {
+        // stub for child classes
+    }
+
+    move_left() {
+        let new_pos = this.blocks_pos.slice(0);
+
+        for (let i = 0; i < 4; i++) {
+            new_pos[i][0]--;
+        }
+
+        if (!this.check_valid_move(new_pos)) return;
+
+        this.blocks_pos = new_pos;
+        this.draw();
+    }
+
+    move_right() {
+        let new_pos = this.blocks_pos.slice(0);
+
+        for (let i = 0; i < 4; i++) {
+            new_pos[i][0]++;
+        }
+
+        if (!this.check_valid_move(new_pos)) return;
+
+        this.blocks_pos = new_pos;
+        this.draw();
+    }
+
+    move_down() {
+        let new_pos = this.blocks_pos.slice(0);
+
+        for (let i = 0; i < 4; i++) {
+            new_pos[i][1]--;
+        }
+
+        if (!this.check_valid_move(new_pos)) return;
+
+        this.blocks_pos = new_pos;
+        this.draw();
+    }
+
+    rotate_cw() { }
+
+    rotate_ccw() { }
+
+    hard_drop() {
+        // this should be common for all blocks
+    }
+
+    check_valid_move(new_pos) { }
+
+}
+
+class I_Block extends Piece {
+    constructor() {
+        super();
+        // spawning the I block horizontally at maximum height
+        this.blocks_pos = [[3, 19], [4, 19], [5, 19], [6, 19]];
+    }
+
+    draw() {
+        ctx.fillStyle = "#00f9ff"
+        for (let i = 0; i < 4; i++) {
+            x = this.blocks_pos[i][0];
+            y = this.blocks_pos[i][1];
+            ctx.fillRect(cols * x, rows * y, )
+        }
+    }
+}
---
+++ tetris.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const WIDTH = 10;
+const HEIGHT = 20;
+
+const rows = canvas.height / HEIGHT;
+const cols = canvas.width / WIDTH;
+
+// i miss my int main void boys
+
+setup_board();
+// play();
+
+ctx.fillStyle = "#FFFFFF";
+ctx.fillRect(1, 0.25, 29, 29/4);
+
+
+////////////////////////////////////////////////////////////////////////////////
+/////// helper functions
+
+// setting up the display of the board
+function setup_board() {
+    console.log("Hello there?")
+
+    for (var i = 1; i < 11; i++) {
+        draw_line(cols * i, 0)
+    }
+    // probs chuck a welcome screen or some shit
+}
+
+// the game logic
+function play() {
+
+}
+
+/////// the actal helper functions
+
+// draws the vertical alignment lines on the board
+function draw_line(x, y) {
+    ctx.fillStyle = "#5e5e5e";
+    ctx.fillRect(x, y, 1, 600);
+}
+
+// manages the spawning of the pieces
+function spawner()
+{
+    // stub for now
+}
